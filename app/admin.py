319a1f3a050111443cab912878515536d88b161b
William Leal <wsleal@yahoo.com>
2024-12-28 11:02:30 -0300

    update admin.py

--- app/admin.py
+++ app/admin.py
@@ -11,3 +11,12 @@ class ServerAdmin(admin.ModelAdmin):
 class PeerAdmin(admin.ModelAdmin):
     readonly_fields = ("address",)
     change_form_template = "custom_admin/change_form.html"
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "server":
+            if db_field.blank:
+                kwargs["queryset"] = models.Server.objects.filter(name="123")
+
+            if db_field.model:
+                print("ok")
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
