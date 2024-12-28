01b3d5c05577039f24dfd9069049390ce8c99a65
William Leal <wsleal@yahoo.com>
2024-12-28 07:56:43 -0300

    update urls.py

--- core/urls.py
+++ core/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("app.urls")),
+    path("app/", include("app.urls")),
+    path("", RedirectView.as_view(url="admin/")),
 ]
