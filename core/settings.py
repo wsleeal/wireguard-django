e63703b3795b72b4f3705c978508b1c178557107
William Leal <wsleal@yahoo.com>
2024-12-29 22:44:18 -0300

    update settings.py

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-bg-zptpz3q)pcuqsk_3@$5sdnhp^p)#ilker66qi628^v8-l2s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
