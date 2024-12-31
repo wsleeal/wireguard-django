294ab5b3d55388826dc76e943a9379460347da5d
William Leal <wsleal@yahoo.com>
2024-12-31 17:53:57 -0300

    update settings.py

--- core/settings.py
+++ core/settings.py
@@ -208,11 +208,17 @@ JAZZMIN_SETTINGS = {
     # Whether to aut expand the menu
     "navigation_expanded": True,
     # Hide these apps when generating side menu e.g (auth)
-    "hide_apps": [],
+    "hide_apps": ["auth"],
     # Hide these models when generating side menu (e.g auth.user)
-    "hide_models": [],
+    "hide_models": ["auth.user", "auth.Group"],
     # List of apps (and/or models) to base side menu ordering off of (does not need to contain all apps/models)
-    "order_with_respect_to": ["auth", "app", "app.Server", "app.Peer", "app.PeerStatus"],
+    "order_with_respect_to": [
+        "auth",
+        "app",
+        "app.PeerStatus",
+        "app.Peer",
+        "app.Server",
+    ],
     # Custom links to append to app groups, keyed on app name
     # "custom_links": {"books": [{"name": "Make Messages", "url": "make_messages", "icon": "fas fa-comments", "permissions": ["books.view_book"]}]},
     # Custom icons for side menu apps/models See https://fontawesome.com/icons?d=gallery&m=free&v=5.0.0,5.0.1,5.0.10,5.0.11,5.0.12,5.0.13,5.0.2,5.0.3,5.0.4,5.0.5,5.0.6,5.0.7,5.0.8,5.0.9,5.1.0,5.1.1,5.2.0,5.3.0,5.3.1,5.4.0,5.4.1,5.4.2,5.13.0,5.12.0,5.11.2,5.11.1,5.10.0,5.9.0,5.8.2,5.8.1,5.7.2,5.7.1,5.7.0,5.6.3,5.5.0,5.4.2
